8dbde6934e9d147539c722ce8761c906f7337a44
JonathanQuiles <30734439+JonathanQuiles@users.noreply.github.com>
2018-01-11 17:20:04 -0600

    Add files via upload

---
+++ angular.js
@@ -0,0 +1,14 @@
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope, $http){
+    
+    
+    $http.get('js/data.json').then(function(response){
+        $scope.usuarios = response.data;
+        console.log($scope.usuarios);
+    });
+     
+
+    
+});
+
+
